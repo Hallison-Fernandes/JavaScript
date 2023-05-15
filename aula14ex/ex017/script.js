acc812bd8e6f8e4cb7a9366ddec52c3b910c9002
Hallison Fernandes <133378183+Hallison-Fernandes@users.noreply.github.com>
2023-05-15 16:12:39 -0300

    ultimos ajustes antes da correção

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,13 +1,13 @@
 function gerar() {
     let num = document.querySelector('input#num')
-    let tab = document.querySelector('select#tabela')
+    let tab = document.querySelector('div#tabela')
     //alert(Number(num.value))
 
     if (Number(num.value.length) === 0) {
         alert('[ERRO] Digite um número')
     } else {
         for (let r = 1; r = 10; r++) {
-            alert()
+            tab.innerHTML += `${Number(num.value) } X ${r} = ${Number(num.value) * r} <br>`
         }
     }
 }
